f85d89979a9226cf4ff4ba8ba54de9a22ad989e8
Maurits Evers <maurits.evers@gmail.com>
2016-06-14 09:08:15 +1000

    Updated MS data-based plots

--- MS/script.R
+++ MS/script.R
@@ -1,4 +1,4 @@
-# Read in data
+# Read IBAQ data
 fn <- c("../interactome/160511_proteinGroups_IBAQ_interactome.tsv",
         "../interactome/160511_proteinGroups_IBAQ_totalproteome.tsv");
 d <- sapply(fn, function(x) read.delim(x, sep = "\t", header = TRUE, stringsAsFactors = FALSE));
@@ -56,37 +56,101 @@ hist(data[, 1], breaks = 20,
      xlab = "log10 IBAQ",
      ylab = "Abundance",
      xlim = c(0, 10),
-     main = sprintf("%s\nmean(log10 IBAQ) = %4.3f", colnames(data)[1], mean(data[, 1])),
+     main = sprintf("IBAQ from %s data\nmean(log10 IBAQ) = %4.3f, N = %i",
+         colnames(data)[1], mean(data[, 1]), length(data[, 1])),
      font.main = 1);
 hist(data[, 2], breaks = 20,
      xlab = "log10 IBAQ",
      ylab = "Abundance",
      xlim = c(0, 10),
-     main = sprintf("%s\nmean(log10 IBAQ) = %4.3f", colnames(data)[2], mean(data[, 2])),
+     main = sprintf("IBAQ from %s data\nmean(log10 IBAQ) = %4.3f, N = %i",
+         colnames(data)[2], mean(data[, 2]), length(data[, 2])),
      font.main = 1);
 ttest1 <- t.test(data[, 1], data[, 2]);
 ttest2 <- t.test(data[, 1] - mean(data[, 1]), data[, 2] - mean(data[, 2]));
 dev.off();
 
-
-pdf("densityplot.pdf", width = 14);
+# Density plot
+pdf("densityplot_interactome_proteome.pdf", width = 14);
 par(mfrow = c(1, 2));
-plot(density(data[, 1]), col = "black",
+plot(density(data[, 1]), col = "red",
      xlab = "log10 IBAQ",
      xlim = c(0, 10),
      ylim = c(0, 0.5),
+     lwd = 2,
      main = colnames(data)[1],
      font.main = 1);
-lines(density(IBAQ[[1]][, 2]), col = "red");
-legend("topleft", c("all proteins in interactome", "proteins in interactome and proteome "),
-       col = c("red", "black"), lwd = c(1, 1), bty = "n");
-plot(density(data[, 2]), col = "black",
+lines(density(IBAQ[[1]][, 2]), col = "black", lwd = 2);
+legend("topleft",
+       c(sprintf("all proteins in interactome (N = %i)",
+                 length(IBAQ[[1]][, 2])),
+         sprintf("proteins in interactome and proteome (N = %i)",
+                 length(data[, 1]))),
+       col = c("black", "red"), lwd = c(2, 2), bty = "n");
+plot(density(data[, 2]), col = "red",
      xlab = "log10 IBAQ",
      xlim = c(0, 10),
      ylim = c(0, 0.5),
+     lwd = 2,
      main = colnames(data)[2],
      font.main = 1);
-lines(density(IBAQ[[2]][, 2]), col = "red");
-legend("topleft", c("all proteins in proteome", "proteins in proteome and interactome"),
-       col = c("red", "black"), lwd = c(1, 1), bty = "n");
+lines(density(IBAQ[[2]][, 2]), col = "black", lwd = 2);
+legend("topleft",
+       c(sprintf("all proteins in proteome (N = %i)",
+                 length(IBAQ[[2]][, 2])),
+         sprintf("proteins in proteome and interactome (N = %i)",
+                 length(data[, 2]))),
+       col = c("black", "red"), lwd = c(2, 2), bty = "n");
+dev.off();
+
+
+# Read protein IDs for different groups
+# Need to do manually, because of different file formats
+groupIDs <- list();
+groupIDs[[length(groupIDs) + 1]] <- as.vector(IBAQ[[2]][, 1]);
+groupIDs[[length(groupIDs) + 1]] <- as.vector(read.table("../interactome/GO_RNAbinding.tsv",
+                                               header = FALSE)[, 2]);
+groupIDs[[length(groupIDs) + 1]] <- as.vector(read.delim("../interactome/ss_candidates.tsv",
+                                               header = TRUE)[, 3]);
+groupIDs[[length(groupIDs) + 1]] <- as.vector(read.delim("../interactome/ns_candidates.tsv",
+                                               header = TRUE)[, 3]);
+groupIDs[[length(groupIDs) + 1]] <- as.vector(read.delim("../interactome/unknown_RBD.tsv",
+                                               header = TRUE)[, 2]);
+names(groupIDs) <- c(names(IBAQ)[2],
+                     "GO:RNA binding (from proteome)",
+                     "ss candidates (interactome)",
+                     "ns candidates (interactome)",
+                     "unknown RBD (interactome)");
+
+
+# Density plot of IBAQ values based on proteome data,
+# split up into different groups
+library(RColorBrewer);
+pdf("densityplot_proteome.pdf", width = 7);
+par(mfrow = c(1, 1));
+plot(density(IBAQ[[2]][, 2]), col = "black",
+     xlab = "log10 IBAQ",
+     xlim = c(0, 10),
+     ylim = c(0, 0.6),
+     lwd = 2,
+     main = "log10 IBAQ values from proteome data for groups as indicated",
+     font.main = 1);
+alpha <- 255;
+col <- c(
+    rgb(95, 178, 51, alpha = alpha, maxColorValue = 255),
+    rgb(106, 127, 147, alpha = alpha, maxColorValue = 255),
+    rgb(245, 114, 6, alpha = alpha, maxColorValue = 255),
+    rgb(235, 15, 19, alpha = alpha, maxColorValue = 255),
+    rgb(143, 47, 139, alpha = alpha, maxColorValue = 255),
+    rgb(19, 150, 219, alpha = alpha, maxColorValue = 255));
+col <- col[1:(length(groupIDs) - 1)];
+for (i in 2:length(groupIDs)) {
+    lines(density(IBAQ[[2]][which(IBAQ[[2]][, 1] %in% groupIDs[[i]]), 2]),
+          col = col[i - 1], type = "l", lwd = 2);
+}
+legend("topleft",
+       sprintf("%s (N = %i)", names(groupIDs), sapply(groupIDs, length)),
+       col = c("black", col),
+       lwd = rep(2, length(groupIDs)),
+           bty = "n");
 dev.off();
